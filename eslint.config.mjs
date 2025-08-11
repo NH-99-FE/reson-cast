0e5d73045668739cbe20f6ed4d18ac07cad7d36a
NH-99-FE <lianglh_only@163.com>
2025-08-11 21:19:25 +0800

    feat(project): ✨  project init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  { ignores: ['**/node_modules/**', '**/.next/**', 'dist/**'] },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: { 'simple-import-sort': eslintPluginSimpleImportSort },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      // "no-console": "error",
+    },
+  },
+  eslintConfigPrettier,
+]
 
-export default eslintConfig;
+export default eslintConfig
