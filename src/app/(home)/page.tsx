013021a25cf211f338ce1cf2a756e803f72a3bc7
NH-99-FE <lianglh_only@163.com>
2025-08-13 17:07:54 +0800

    build(other): 🛠  tRPC setup
    安装并初步配置tRPC

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>content</div>
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
+import { HydrateClient, trpc } from '@/trpc/server'
+
+import { PageClient } from './client'
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: 'jack' })
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <PageClient />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  )
 }
