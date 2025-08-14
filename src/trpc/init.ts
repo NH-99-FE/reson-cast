768c1cafe42442fa046f66d1c12911b630135d5d
NH-99-FE <lianglh_only@163.com>
2025-08-14 10:38:58 +0800

    build(other): 🛠  tRPC配置
    在trpc上下文添加认证信息，添加propectedProceduce（中间件：认证+速率限制+用户注入）。

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,7 +1,12 @@
 import { auth } from '@clerk/nextjs/server'
 import { initTRPC, TRPCError } from '@trpc/server'
+import { eq } from 'drizzle-orm'
 import { cache } from 'react'
 import superjson from 'superjson'
+
+import { db } from '@/db'
+import { users } from '@/db/schema'
+import { ratelimit } from '@/lib/ratelimit'
 
 export const createTRPCContext = cache(async () => {
   /**
@@ -27,15 +32,27 @@ const t = initTRPC.context<Context>().create({
 export const createTRPCRouter = t.router
 export const createCallerFactory = t.createCallerFactory
 export const baseProcedure = t.procedure
+
 export const protectedProcedure = t.procedure.use(async function isAuthed(opts) {
   const { ctx } = opts
   if (!ctx.clerkUserId) {
     throw new TRPCError({ code: 'UNAUTHORIZED' })
   }
+  const [user] = await db.select().from(users).where(eq(users.clerkId, ctx.clerkUserId)).limit(1)
+
+  if (!user) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' })
+  }
+
+  const { success } = await ratelimit.limit(user.id)
+  if (!success) {
+    throw new TRPCError({ code: 'TOO_MANY_REQUESTS' })
+  }
 
   return opts.next({
     ctx: {
       ...ctx,
+      user,
     },
   })
 })
