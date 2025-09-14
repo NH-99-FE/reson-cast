58f1c685a66da2ebb414e13d09a37bf69228e049
NH-99-FE <lianglh_only@163.com>
2025-09-14 22:34:42 +0800

    feat(dev): ✨  为搜索结果页面添加骨架屏

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
 import { InfiniteScroll } from '@/components/infinite-scroll'
 import { DEFAULT_LIMIT } from '@/constants'
 import { useIsMobile } from '@/hooks/use-mobile'
-import { VideoGridCard } from '@/modules/videos/ui/components/video-grid-card'
-import { VideoRowCard } from '@/modules/videos/ui/components/video-row-card'
+import { VideoGridCard, VideoGridCardSkeleton } from '@/modules/videos/ui/components/video-grid-card'
+import { VideoRowCard, VideoRowCardSkeleton } from '@/modules/videos/ui/components/video-row-card'
 import { trpc } from '@/trpc/client'
 
 interface ResultsSectionProps {
@@ -13,6 +16,37 @@ interface ResultsSectionProps {
 }
 
 export const ResultsSection = ({ query, categoryId }: ResultsSectionProps) => {
+  return (
+    <Suspense fallback={<ResultsSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>出错了</p>}>
+        <ResultsSectionSuspense query={query} categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+const ResultsSectionSkeleton = () => {
+  const isMobile = useIsMobile()
+  return (
+    <>
+      {isMobile ? (
+        <div className="flex flex-col gap-4 gap-y-10">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <VideoGridCardSkeleton key={index} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <VideoRowCardSkeleton key={index} />
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
   const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
     { query, categoryId, limit: DEFAULT_LIMIT },
     {
