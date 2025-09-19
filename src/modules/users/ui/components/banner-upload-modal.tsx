e822451830c23f9535c064b2df12bb9148134b5f
NH-99-FE <lianglh_only@163.com>
2025-09-19 14:02:04 +0800

    feat(dev): ✨  实现上传个人信息页面背景图功能

---
+++ src/modules/users/ui/components/banner-upload-modal.tsx
@@ -0,0 +1,22 @@
+import { ResponsiveModal } from '@/components/responsive-modal'
+import { UploadDropzone } from '@/lib/uploadthing'
+import { trpc } from '@/trpc/client'
+
+interface BannerUploadModalProps {
+  userId: string
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+export const BannerUploadModal = ({ userId, onOpenChange, open }: BannerUploadModalProps) => {
+  const utils = trpc.useUtils()
+  const onUploadComplete = () => {
+    utils.users.getOne.invalidate({ id: userId })
+    onOpenChange(false)
+  }
+  return (
+    <ResponsiveModal open={open} title="上传背景图" onOpenChange={onOpenChange}>
+      <UploadDropzone endpoint="bannerUploader" onClientUploadComplete={onUploadComplete} />
+    </ResponsiveModal>
+  )
+}
--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -1,10 +1,12 @@
 import { useAuth } from '@clerk/nextjs'
 import { Edit2Icon } from 'lucide-react'
+import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import { cn } from '@/lib/utils'
 import { userGetOneOutput } from '@/modules/users/types'
+import { BannerUploadModal } from '@/modules/users/ui/components/banner-upload-modal'
 
 interface UserPageBannerProps {
   user: userGetOneOutput
@@ -16,20 +18,26 @@ export const UserPageBannerSkeleton = () => {
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
   const { userId } = useAuth()
+  const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false)
+  console.log(user)
   return (
     <div className="group relative">
+      <BannerUploadModal userId={user.id} open={isBannerUploadModalOpen} onOpenChange={setIsBannerUploadModalOpen} />
       <div
         className={cn(
-          'h-[25vh] max-h-[200px] w-full rounded-xl bg-gradient-to-r from-gray-100 to-gray-200',
-          user.bannerUrl ? 'bg-cover bg-center' : 'bg-gray-100'
+          'h-[25vh] max-h-[200px] w-full rounded-xl',
+          user.bannerUrl ? 'bg-cover bg-center' : 'bg-gradient-to-r from-gray-100 to-gray-200'
         )}
+        style={user.bannerUrl ? { backgroundImage: `url(${user.bannerUrl})` } : undefined}
       >
         {user.clerkId === userId && (
           <Button
             type="button"
             size="icon"
             className="absolute top-4 right-4 rounded-full bg-black/40 opacity-100 transition-opacity duration-300 group-hover:opacity-100 hover:bg-black/50 md:opacity-0"
-            onClick={() => {}}
+            onClick={() => {
+              setIsBannerUploadModalOpen(true)
+            }}
           >
             <Edit2Icon className="size-4 text-white" />
           </Button>
