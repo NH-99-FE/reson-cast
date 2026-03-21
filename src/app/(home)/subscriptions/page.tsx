6d25b0b0812e3ab7bffd843c64db1e434b55642a
NH-99-FE <lianglh_only@163.com>
2026-03-21 10:50:18 +0800

    fix(project): 🐛  强制动态渲染

--- src/app/(home)/subscriptions/page.tsx
+++ src/app/(home)/subscriptions/page.tsx
@@ -2,6 +2,8 @@ import { DEFAULT_LIMIT } from '@/constants'
 import { SubscriptionsView } from '@/modules/subscriptions/ui/views/subscriptions-view'
 import { HydrateClient, trpc } from '@/trpc/server'
 
+export const dynamic = 'force-dynamic'
+
 const Page = async () => {
   void trpc.subscriptions.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT })
 
